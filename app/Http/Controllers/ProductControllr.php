0e6025dff9e72aff1d0c54719fc60f4a513c4f41
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-07-04 14:32:43 +0545

    Setting up index and store methods

--- app/Http/Controllers/ProductControllr.php
+++ app/Http/Controllers/ProductControllr.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductControllr extends Controller
 {
     public function index(){
+        $products = Product::orderBy('year', 'ASC')->get();
+        return response()->json($products);
+    }
 
+    public function store(Request $request){
+        $product = new Product();
+        $product->name = $request->name;
+        $product->year = $request->year;
+        $product->price = $request->price;
+        $product->save();
+        return response()->json(['success' => 'The Products Was Created Successfully']);
     }
 }
